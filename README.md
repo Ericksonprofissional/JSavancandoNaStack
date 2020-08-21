538cf7f019bd236c59cd8fedb12a32cf5af539eb
ericksonprofissional <ericksonprofissional@gmail.com>
2020-08-20 21:49:57 -0400

    Herança

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ Boas práticas na hora de declarar atributos
 Declarando sempre as propriedades como sendo privadas inicialmente
 Problemas de duplicação de código
 Problemas de código extremamente compartilhado
+
+Aula2
+O que aprendemos nessa aula:
+
+Especializando código
+
+Herdando propriedades e atributos
+
+Sobrecarga de métodos
+
+Compartilhando código de forma segura
+
+Mantendo nosso código na linguagem do cliente
+
+Evitando código muito complicado através da herança
+
+Palavra-chave Super
+
+Método e propriedades privadas e protegidas
--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
-import { Conta } from "./Conta.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 const cliente2 = new Cliente("Alice", 88822233309);
 
 
-const contaCorrenteRicardo = new Conta(0, 1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const conta1 = new ContaCorrente(cliente1, 1001);
+conta1.depositar(500);
+conta1.sacar(50);
 
-const conta2 = new ContaCorrente(102, cliente2);
-const contaPoupanca1 = new Conta(1000, cliente1, contaCorrenteRicardo.agencia)
+const conta2 = new ContaCorrente(cliente2, 102);
+const contaPoupanca1 = new ContaPoupanca(1000, cliente1, conta1._agencia)
 
 let valor = 200;
-contaCorrenteRicardo.tranferir(valor, conta2);
-
-console.log(contaCorrenteRicardo);
+conta1.tranferir(valor, conta2);
+conta2.sacar(10)
+console.log(conta2);
+console.log(conta1);
 console.log(contaPoupanca1);
